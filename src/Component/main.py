69b49c214e5f8c713cd182a444bb521b770c7880
vasco0603 <michaelharyanto1688@gmail.com>
2023-10-14 13:32:31 +1100

    backend half done

---
+++ src/Component/main.py
@@ -0,0 +1,53 @@
+# Backend Script Example
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from neo4j import GraphDatabase
+
+# for security reasons
+# you can store your database information in a separate file
+uri = "neo4j+s://19b5ded5.databases.neo4j.io"
+user = "neo4j"
+password = "eS87GcULkCOLvQeqHzIiRtftod6AbfkzhPCcuOx_iUY"
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def mainpage():
+    return ("backend connected successfully")
+
+@app.get("/WalletDetails")
+async def walletdetails():
+    driver = GraphDatabase.driver(uri, auth=(user, password))
+    session = driver.session(database="neo4j")
+
+
+    query = "MATCH p=(source:wallet {addressId: '0x8d08aad4b2bac2bb761ac4781cf62468c9ec47b4'})-[:SENT_TO]->(target) return p;"
+
+    result = session.run(query)
+
+     # Initialize an empty list to store results
+    results_list = []
+
+    # Iterate over the query results and append them to the list
+    for record in result:
+        results_list.append(record)
+
+    # Close the session and driver when done
+    session.close()
+    driver.close()
+
+    return results_list
+
+
