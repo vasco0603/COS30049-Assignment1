26a8c14de8b4bcd2754f70dd515d068e67b8f171
vasco0603 <michaelharyanto1688@gmail.com>
2023-11-02 22:54:10 +1100

    fix

---
+++ README.txt
@@ -0,0 +1,15 @@
+README-----
+
+BEFORE RUNNING THE PROJECT, PLEASE MAKE SURE TO DO THESE FOLLOWING THINGS
+
+1. CREATE CONDA ENVIRONMENT
+2. RUN THESE COMMANDS:
+	conda install -c conda-forge fastapi
+	conda install -c conda-forge uvicorn-standard
+	conda install -c conda-forge neo4j-python-driver
+	pip install neo4j
+
+3. RUN uvicorn main:app --reload
+4. OPEN CLI AND DIRECT TO PROJECT DIRECTORY
+5. RUN npm install
+6. RUN npm start
--- src/Component/main.py
+++ src/Component/main.py
@@ -38,87 +38,43 @@ async def walletdetails(addressId: str):
 
     # Cypher query to retrieve wallet details and related relationships
     query = (
-        "MATCH (a:wallet {addressId: '" + addressId + "'})-[:SENT_TO]->(b:wallet) "
-        "RETURN a, b, [(a)-[r:SENT_TO]->(b) | { _nodes: [a, b], _relationships: r }] AS relationships;"
+        'MATCH p=((w:wallet {addressId: "'+addressId+'"})-[:SENT_TO]->()) RETURN p;'
     )
+    print(query)
 
     result = session.run(query)
 
     results_list = []
-
     for record in result:
-        relationships = record["relationships"]
-
-        # Append each relationship to the result list
-        for relationship in relationships:
-            results_list.append([relationship])
-
-    formatted_result = []
-
-    for item in results_list:
-        formatted_item = {
-            "_nodes": item[0]["_nodes"],
-            "_relationships": [{
-                "gas_price": item[0]["_relationships"]["gas_price"],
-                "block_timestamp": item[0]["_relationships"]["block_timestamp"],
-                "gas_used": item[0]["_relationships"]["gas_used"],
-                "gas": item[0]["_relationships"]["gas"],
-                "transaction_fee": item[0]["_relationships"]["transaction_fee"],
-                "block_number": item[0]["_relationships"]["block_number"],
-                "value": item[0]["_relationships"]["value"],
-                "hash": item[0]["_relationships"]["hash"]
-            }]
-        }
-        formatted_result.append([formatted_item])
+        results_list.append(record)
 
     session.close()
     driver.close()
-    return formatted_result
+    print(results_list)
+    return results_list
 
 # Define a route for retrieving wallet details from a specific address
 @app.get("/WalletFrom")
-async def walletdetailsfrom(addressId: str):
+async def walletdetails(addressId: str):
     driver = GraphDatabase.driver(uri, auth=(user, password))
     session = driver.session(database="neo4j")
 
-    # Cypher query to retrieve wallet details and related relationships from a specific address
+    # Cypher query to retrieve wallet details and related relationships
     query = (
-        "MATCH (a:wallet)-[:RECEIVED_FROM]->(b:wallet {addressId: '" + addressId + "'}) "
-        "RETURN a, b, [(a)-[r:RECEIVED_FROM]->(b) | { _nodes: [a, b], _relationships: r }] AS relationships;"
+        'MATCH p=((w:wallet)<-[:RECEIVED_FROM]-(x:wallet {addressId: "'+addressId+'"})) RETURN p;'
     )
+    print(query)
 
     result = session.run(query)
 
     results_list = []
-
     for record in result:
-        relationships = record["relationships"]
-
-        # Append each relationship to the result list
-        for relationship in relationships:
-            results_list.append([relationship])
-
-    formatted_result = []
-
-    for item in results_list:
-        formatted_item = {
-            "_nodes": item[0]["_nodes"],
-            "_relationships": [{
-                "gas_price": item[0]["_relationships"]["gas_price"],
-                "block_timestamp": item[0]["_relationships"]["block_timestamp"],
-                "gas_used": item[0]["_relationships"]["gas_used"],
-                "gas": item[0]["_relationships"]["gas"],
-                "transaction_fee": item[0]["_relationships"]["transaction_fee"],
-                "block_number": item[0]["_relationships"]["block_number"],
-                "value": item[0]["_relationships"]["value"],
-                "hash": item[0]["_relationships"]["hash"]
-            }]
-        }
-        formatted_result.append([formatted_item])
+        results_list.append(record)
 
     session.close()
     driver.close()
-    return formatted_result
+    print(results_list)
+    return results_list
 
 # Define a route for retrieving details of all wallets (limited to 25)
 @app.get("/allwallet")
